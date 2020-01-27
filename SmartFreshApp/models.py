0f67ee8376bdcc276f187b05623e94dcbdb9361b
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2020-01-27 20:05:11 +0100

    Test DB

--- SmartFreshApp/models.py
+++ SmartFreshApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Container(models.Model):
+    numLot = models.IntegerField(default=0)
+    numContainer = models.CharField(max_length=16)
+    isSQ = models.BooleanField(default=0)
+    isC = models.BooleanField(default=0)
+    isF = models.BooleanField(default=0)
+    commentaires = models.CharField(max_length=128)
+
+    def __str__(self):
+        return str(self.numLot) + ' - ' + str(self.numContainer)
+
+    
+    
